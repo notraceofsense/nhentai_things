4ac138cc7d230c8116f854dbfdef43bff3bc43ea
notraceofsense <56681804+notraceofsense@users.noreply.github.com>
2020-08-24 16:35:43 -0400

    Updated nhentai.py: Added multithreading

--- nhentai.py
+++ nhentai.py
@@ -2,7 +2,7 @@
 # A program for downloading stuff from nhentai using the numbers.
 # I was pretty tired when I originally wrote this, thus the code is kinda jank.
 # Rewriting to add some cool stuff and make things better.
-# TODO: Batch jobs (DONE), multithreading, implement saving jsons, idk.
+# TODO: multithreading (Book done), implement saving jsons, idk.
 # Excessively commented so that almost anyone can understand.
 # usage: nhentai.py <digits> -p/--path <Path to folder> -j/--json (not done)
 
@@ -10,7 +10,53 @@
 # with stuff such as HTTP and writing to disk.
 # I can't remember if requests is installed by default or not.
 # If it isn't, https://requests.readthedocs.io/en/master/user/install/
-import requests, sys, os, argparse
+import requests, sys, os, argparse, threading
+
+# Moved the book-getting method into a function for multithreading.
+def get_book(n):
+	# Add the book id to the url so we can access the API
+	api_url = "https://nhentai.net/api/gallery/{}".format(n)
+
+	# get the JSON so we can get the media id.
+	print("{}: Getting metadata from nhentai...".format(n))
+	book_json = requests.get(api_url).json()
+
+	# Get the media id from the json so we can access the gallery.
+	media_id = book_json['media_id']
+	printBook(n, "Media ID: {}".format(media_id))
+
+	# add the media id to the url
+	gallery_url = "https://i.nhentai.net/galleries/{}".format(media_id)
+
+	# Make the directory.
+	printBook(n, "Making directory...")
+	current_dir = os.path.join(path, str(n))
+	os.mkdir(current_dir)
+
+	# Here's where the jank begins!
+	i = 0
+	f = 1
+
+	printBook(n, "Downloading...")
+
+	# For all the pages, get the image type and download them.
+	for x in book_json["images"]["pages"]:
+		filename = "{}.{}".format(
+			f, getImageType(book_json["images"]["pages"][i]["t"]))
+		open("{}\{}".format(current_dir, filename), "xb").write(requests.get(
+			"{}/{}".format(gallery_url, filename), allow_redirects=True
+			).content)
+
+		printBook(n, "{} downloaded.".format("{}/{}".format(gallery_url, filename)))
+    
+		i += 1
+		f += 1
+
+	printBook(n, "Finished downloading.")
+
+# Function that appends book ID to debug messages
+def printBook(n, m):
+	print("{}: {}".format(n, m))
 
 # A function for getting the full file extentions from the abbreviations in the
 # JSON.
@@ -44,45 +90,10 @@ path = args.get('path')
 
 # For loop, yay batch jobs!
 for n in id_list:
-	# Print the book being downloaded.
-	print(n)
+	# Yay, multithreading!
+	threading.Thread(target=get_book, args=(n,)).start()
 
-	# Add the book id to the url so we can access the API
-	api_url = "https://nhentai.net/api/gallery/{}".format(n)
-
-	# get the JSON so we can get the media id.
-	print("\tGetting metadata from nhentai...")
-	book_json = requests.get(api_url).json()
-
-	# Get the media id from the json so we can access the gallery.
-	media_id = book_json['media_id']
-	print("\tMedia ID: {}".format(media_id))
-
-	# add the media id to the url
-	gallery_url = "https://i.nhentai.net/galleries/{}".format(media_id)
-
-	# Make the directory.
-	print("\tMaking directory...")
-	current_dir = os.path.join(path, str(n))
-	os.mkdir(current_dir)
-
-	# Here's where the jank begins!
-	i = 0
-	f = 1
-
-	print("\tDownloading...")
-
-	# For all the pages, get the image type and download them.
-	for x in book_json["images"]["pages"]:
-		filename = "{}.{}".format(
-			f, getImageType(book_json["images"]["pages"][i]["t"]))
-		open("{}\{}".format(current_dir, filename), "xb").write(requests.get(
-			"{}/{}".format(gallery_url, filename), allow_redirects=True
-			).content)
-
-		print("\t\t{} downloaded.".format("{}/{}".format(gallery_url, filename)))
-    
-		i += 1
-		f += 1
-
-	print("\tFinished downloading.")
+for t in threading.enumerate():
+	if t == threading.current_thread():
+		continue
+	t.join()
